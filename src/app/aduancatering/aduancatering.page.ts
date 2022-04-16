8e9157bdc9fa11c83c2981d46bc013610570daaa
ionic5jwt <asaservicelife@gmail.com>
2022-04-17 03:28:17 +0800

    POST aduancatering

--- src/app/aduancatering/aduancatering.page.ts
+++ src/app/aduancatering/aduancatering.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
- import { HttpClient } from '@angular/common/http';
- 
+ import { HttpClient, HttpHeaders } from '@angular/common/http';
+ import { tap, timeout } from 'rxjs/operators';
+import { observable, ReplaySubject } from 'rxjs';
+import { Platform, ToastController } from '@ionic/angular';
+
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-aduancatering',
@@ -8,21 +13,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aduancatering.page.scss'],
 })
 
-export class AduancateringPage implements OnInit {
-  constructor(private http:HttpClient){}
-  onSubmit(data: any)
-
-  {
-    this.http.post('http://asabeta.com/api/catering/add',data)
-    .subscribe((result)=>{
-console.warn('result',result)
-    })
-    console.warn(data);
-  }
-
-  
-
-  ngOnInit() {
-  
-  }
+export class AduancateringPage {
+  [x: string]: any;
+  Data:any;
+  DataResponse:any;
+  DataCheckLogin:any;
+  authenticationState = new ReplaySubject();
+  token:any;
+  API_URL = 'http://asabeta.com/api/';
+  TOKEN_KEY = 'accesstoken';
+  REFRESH_TOKEN_KEY = 'refreshtoken';
+  ROLE = 'role';
+  server: string;
+ constructor(
+  private http: HttpClient,
+  private platform: Platform,
+  public toastController: ToastController
+) {
+  this.platform.ready().then(() => {
+    this.onSubmit();
+  });
+ }
+ options(Data: string, options: any) {
+  throw new Error('Method not implemented.');
 }
+onSubmit(){
+let dataStorage=JSON.parse(localStorage.getItem(this.TOKEN_KEY));
+this.token=dataStorage;
+console.log("token : " +this.token);
+const headers = new HttpHeaders({
+  'Content-Type': 'application/json',
+  'Authorization': "Bearer "+ this.token
+});
+return this.http.post(this.API_URL + 'catering/add',  {headers: headers, observe: 'response' }).pipe(
+  timeout(8000),
+  tap(Data => {
+    this.Data=Data;
+    if(Data==200){
+    localStorage.setItem(this.TOKEN_KEY, JSON.stringify(this.DataLogin.access_token));
+  }else{
+    this.authenticationState.next(false);
+  }
+  return Data;
+}),
+catchError((err, caught) => {
+  let message = "error";
+
+  if(err.status==400){
+    message='Data Aduan Belum Lengkap.';
+  } else if(err.status==401){
+    message='Data Aduan Belum Ada.';
+  } else {
+    message='Tidak ada koneksi internet. Silakan periksa koneksi Anda.';
+  }
+  return throwError(message);
+})
+
+);
+}
+
+} 
+
+
